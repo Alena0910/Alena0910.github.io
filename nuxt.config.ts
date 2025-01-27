32f701f266d77ecc8a04dc9bb25cf7ca439a5e1d
Huang Pin-Chen <116648694+Alena0910@users.noreply.github.com>
2025-01-28 07:47:58 +0800

    Feat: add workflow  deploy

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -15,8 +15,6 @@ export default defineNuxtConfig({
       viewport: "width=device-width, initial-scale=1",
       title: "Blog",
     },
-    baseURL: '/Alena0910.github.io/',
-    buildAssetsDir: "/assets/",
   },
   routeRules: {
     "/": {
