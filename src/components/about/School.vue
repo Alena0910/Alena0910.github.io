2d0f0f989de7a740db4c2fcb18da126ce311d824
Huang Pin-Chen <116648694+Alena0910@users.noreply.github.com>
2025-04-17 14:22:19 +0800

    Refactor: improve layout structure in School component for better readability

--- src/components/about/School.vue
+++ src/components/about/School.vue
@@ -1,14 +1,16 @@
 <template>
   <div class="border-b border-gray-300 text-[20px]">學歷</div>
-  <div class="flex flex-col gap-2 ml-4">
-    <div class="flex flex-col gap-2" v-for="(item, index) in data" :key="index">
+  <div class="flex flex-col ml-4">
+    <div class="flex flex-col " v-for="(item, index) in data" :key="index">
       <div class="flex flex-col gap-1 mb-2">
         <div class="text-[16px] font-bold border-b">{{ item.school }}</div>
-        <div class="text-[14px] text-gray-700">{{ item.major }}</div>
-        <div class="text-[14px] text-gray-700">{{ item.degree }}</div>
-        <div class="text-[14px] text-gray-700">{{ item.status }}</div>
-        <div class="text-[12px] text-gray-500 w-full flex justify-end">
-          {{ item.startDate }} - {{ item.endDate }}
+        <div class="flex flex-col gap-1 ml-4">
+          <div class="text-[14px] text-gray-700">{{ item.major }}</div>
+          <div class="text-[14px] text-gray-700">{{ item.degree }}</div>
+          <div class="text-[14px] text-gray-700">{{ item.status }}</div>
+          <div class="text-[12px] text-gray-500 w-full flex justify-end">
+            {{ item.startDate }} - {{ item.endDate }}
+          </div>
         </div>
       </div>
     </div>
