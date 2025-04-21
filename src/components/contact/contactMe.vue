6d4bb065625cd1c29cf51f0c9943eb3a0dec5db5
Huang Pin-Chen <116648694+Alena0910@users.noreply.github.com>
2025-04-21 15:14:54 +0800

    Feat: implement ContactSquare component for improved contact information display

--- src/components/contact/contactMe.vue
+++ src/components/contact/contactMe.vue
@@ -1,63 +1,47 @@
 <template>
-  <div>
-    <div class="flex justify-center items-center mt-10">
-      <Card
-        class="w-full flex flex-col justify-center items-center bg-secondary"
-      >
-        <CardContent
-          class="flex flex-row items-center pt-6 w-full justify-center"
-        >
-          <Mail />
-          <a
-            href="mailto:pinzhenhuang0910@gmail.com"
-            target="_blank"
-            class="break-all text-center ml-2"
-          >
-            {{ EMAIL }}</a
-          >
-        </CardContent>
-        <CardFooter
-          class="flex justify-center px-6 pb-6 gap-6 items-center w-full flex-wrap"
-        >
-          <NuxtLink
-            :to="PERSONAL_WEBSITE.github"
-            target="_blank"
-            class="flex flex-wrap gap-2 items-center justify-center"
-          >
-            <Github class="size-6" />
-            <div class="w-fit">{{ WORD_GITHUB }}</div>
-          </NuxtLink>
-          <NuxtLink
-            :to="PERSONAL_WEBSITE.leetcode"
-            target="_blank"
-            class="flex flex-wrap gap-2 items-center justify-center"
-          >
-            <Code class="size-6" />
-            <div class="w-fit">{{ WORD_LEETCODE }}</div>
-          </NuxtLink>
-          <NuxtLink
-            :to="PERSONAL_WEBSITE.instagram"
-            target="_blank"
-            class="flex flex-wrap gap-2 items-center justify-center"
-          >
-            <Instagram class="size-6" />
-            <div class="w-fit">{{ WORD_INSTAGRAM }}</div>
-          </NuxtLink>
-        </CardFooter>
-      </Card>
+  <div
+    class="flex flex-col items-center justify-center gap-1 bg-secondary rounded-lg"
+  >
+    <h2 class="text-xl font-normal border-b">聯絡資訊</h2>
+    <div class="flex flex-wrap justify-center items-center gap-4">
+      <ContactSquare
+        v-for="(item, index) in info"
+        :key="index"
+        :icon="item.icon"
+        :url="item.url"
+        :label="item.label"
+      />
     </div>
   </div>
 </template>
+
 <script setup lang="ts">
-import { Card, CardContent, CardFooter } from "@/components/ui/card";
 import { Mail, Github, Code, Instagram } from "lucide-vue-next";
-import {
-  EMAIL,
-  WORD_GITHUB,
-  WORD_LEETCODE,
-  WORD_INSTAGRAM,
-  PERSONAL_WEBSITE,
-} from "@/src/utils/constants";
+import { EMAIL, PERSONAL_WEBSITE } from "@/src/utils/constants";
+import ContactSquare from "./ContactSquare.vue";
+
+const info = [
+  {
+    icon: Mail,
+    url: "mailto:" + EMAIL,
+    label: EMAIL,
+  },
+  {
+    icon: Github,
+    url: PERSONAL_WEBSITE.github,
+    label: "Github",
+  },
+  {
+    icon: Code,
+    url: PERSONAL_WEBSITE.leetcode,
+    label: "LeetCode",
+  },
+  {
+    icon: Instagram,
+    url: PERSONAL_WEBSITE.instagram,
+    label: "Instagram",
+  },
+];
 </script>
 
 <style scoped>
